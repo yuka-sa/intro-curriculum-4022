57c816067ee23c43f5b6845605cc0c2de1050cc8
Kenichi Oosaki <kenichi_oosaki@dwango.co.jp>
2022-05-16 13:41:10 +0900

    update

--- models/candidate.js
+++ models/candidate.js
@@ -1,5 +1,5 @@
 'use strict';
-const { sequelize, DataTypes } = require('./sequelize-loader');
+const {sequelize, DataTypes} = require('./sequelize-loader');
 
 const Candidate = sequelize.define(
   'candidates',
