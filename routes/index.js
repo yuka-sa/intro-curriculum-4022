b6768a8cf16e3b69926a8729e2a7976cb5b18888
Sato Yuka <165768544+yuka-sa@users.noreply.github.com>
2024-04-26 11:27:38 +0900

    日付の表示形式の変更

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,13 @@ const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient({ log: [ 'query' ] });
 
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
+const timezone = require('dayjs/plugin/timezone');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault('Asia/Tokyo');
+
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   const title = '予定調整くん';
@@ -11,6 +18,9 @@ router.get('/', async (req, res, next) => {
     const schedules = await prisma.schedule.findMany({
       where: { createdBy: parseInt(req.user.id) },
       orderBy: { updatedAt: 'desc' }
+    });
+    schedules.forEach((schedule) => {
+      schedule.formatteUpdatedAt = dayjs(schedule.updatedAt).tz().format('YYYY/MM/DD HH:mm');
     });
     res.render('index', {
       title: title,
